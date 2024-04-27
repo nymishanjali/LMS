cadf679a2321bfeda024f80001f8dd6d4cd54922
tejakarthik <2200032104@kluniversity.in>
2024-04-27 05:56:39 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,48 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
+import userRoute from "./route/userRoute.js";
+import bookRoute from "./route/bookRoute.js";
+const app = express();
+const corsOptions = {
+  origin: "*",
+};
+
+const mongo = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(
+      "mongodb+srv://library:karthik123@cluster0.k7u4bcx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log(`MongoDB server started successfully!`);
+  } catch (err) {
+    throw err;
+  }
+};
+
+// MIDDLEWARES.
+app.use(cors(corsOptions));
+app.use(express.json()); // To validate json objects.
+app.use("/user", userRoute); // user routes.
+app.use("/books", bookRoute); // book routes.
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    success: false,
+    message: err.message,
+    status: err.status,
+    stack: err.stack,
+  });
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  mongo();
+  console.log(`Port successfully started at ${PORT}`);
+});
